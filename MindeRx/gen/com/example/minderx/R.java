7aa945aad85f5cef7cbb4f2398a6c5dcf5a462ff
Joseph <Jtsessions@crimson.ua.edu>
2014-04-05 02:56:08 -0500

    Renames Manage Patients string to resolve errors

--- MindeRx/gen/com/example/minderx/R.java
+++ MindeRx/gen/com/example/minderx/R.java
@@ -24,27 +24,77 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int ManagePatient_bn=0x7f080005;
-        public static final int action_settings=0x7f080007;
-        public static final int inputType=0x7f080006;
-        public static final int listView1=0x7f080003;
-        public static final int login_button=0x7f080000;
-        public static final int logout_bn=0x7f080004;
-        public static final int password_txt=0x7f080001;
-        public static final int username_txt=0x7f080002;
+        public static final int ListView01=0x7f08000f;
+        public static final int ManagePatient_bn=0x7f080015;
+        public static final int PatientName=0x7f080014;
+        public static final int Patient_Name_txt=0x7f080016;
+        public static final int action_settings=0x7f08001c;
+        public static final int assign_to_bn=0x7f080010;
+        public static final int bp_toggle_bn=0x7f080003;
+        public static final int button1=0x7f080013;
+        public static final int cancel_bn=0x7f08001b;
+        public static final int date_txt=0x7f080019;
+        public static final int done_bn=0x7f08000a;
+        public static final int end_date_txt=0x7f080009;
+        public static final int i_o_toggle_bn=0x7f080007;
+        public static final int login_button=0x7f08000b;
+        public static final int logout_bn=0x7f080000;
+        public static final int password_txt=0x7f08000c;
+        public static final int patient_info_listView=0x7f080012;
+        public static final int patient_name_txt=0x7f080001;
+        public static final int patients_listView=0x7f08000e;
+        public static final int pulse_toggle_bn=0x7f080002;
+        public static final int resp_toggle_bn=0x7f080005;
+        public static final int sao2_toggle_bn=0x7f080004;
+        public static final int start_date_txt=0x7f080008;
+        public static final int submit_bn=0x7f08001a;
+        public static final int temp_toggle_bn=0x7f080006;
+        public static final int time_txt=0x7f080018;
+        public static final int unassign_from_bn=0x7f080011;
+        public static final int username_txt=0x7f08000d;
+        public static final int vitals_template_txt=0x7f080017;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_patient_list=0x7f030002;
+        public static final int activity_graph_patient_info=0x7f030000;
+        public static final int activity_login=0x7f030001;
+        public static final int activity_manage_patients=0x7f030002;
+        public static final int activity_patient_blood_pressure=0x7f030003;
+        public static final int activity_patient_heart_rate=0x7f030004;
+        public static final int activity_patient_info=0x7f030005;
+        public static final int activity_patient_list=0x7f030006;
+        public static final int activity_patient_sa_level=0x7f030007;
+        public static final int activity_patient_temperature=0x7f030008;
+        public static final int activity_patient_vitals=0x7f030009;
+        public static final int activity_vitals_template=0x7f03000a;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int activity_graph_patient_info=0x7f050011;
         public static final int app_name=0x7f050000;
+        public static final int assign_to=0x7f050007;
+        public static final int blood_pressure=0x7f05000f;
+        public static final int cancel=0x7f050005;
+        public static final int graph_done=0x7f050009;
+        public static final int heart_rate=0x7f050010;
         public static final int hello_world=0x7f050002;
+        public static final int i_o=0x7f05000d;
+        public static final int log_out=0x7f050004;
+        public static final int manage_patients=0x7f050003;
+        public static final int pulse=0x7f05000a;
+        public static final int resp=0x7f05000c;
+        public static final int sao2=0x7f05000b;
+        public static final int submit=0x7f050006;
+        public static final int temp=0x7f05000e;
+        public static final int title_activity_patient_blood_pressure=0x7f050017;
+        public static final int title_activity_patient_heart_rate=0x7f050016;
+        public static final int title_activity_patient_sa_level=0x7f050015;
+        public static final int title_activity_patient_temperature=0x7f050014;
+        public static final int title_activity_patient_vitals=0x7f050013;
+        public static final int title_activity_vitals=0x7f050012;
+        public static final int unassign_from=0x7f050008;
     }
     public static final class style {
         /** 
