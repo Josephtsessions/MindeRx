5c03c8c90a6d70bd62a4c2ee2ae809047ca7e998
Joseph <Jtsessions@crimson.ua.edu>
2014-04-06 00:33:34 -0500

    Adds expected ListActivity ListView behavior
    Populates PatientListActivity's ListView with sample data and adds a click
    listener that transitions to the proper Activity after selecting a
    patient.
    
    Additionally removes the incorrect transition from PatientListActivity to
    PatientInfoActivity when pressing the Manage Patients button.

--- MindeRx/src/com/example/minderx/PatientListActivity.java
+++ MindeRx/src/com/example/minderx/PatientListActivity.java
@@ -1,10 +1,17 @@
 package com.example.minderx;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class PatientListActivity extends Activity {
 
@@ -16,19 +23,44 @@ public class PatientListActivity extends Activity {
 		setContentView(R.layout.x_activity_patient_list_relink);
 
 		setupListeners();	
+		
+		ListView patientsListView = (ListView) findViewById(R.id.patients_listView);
+		
+		List<String> patients = new ArrayList<String>();
+		patients.add("Michael Jackson");
+		patients.add("Billy Mays");
+		patients.add("Steve Jobs");
+		patients.add("SomeoneWhoisn'tDead");
+		
+		// Adapters are used to populate ListViews in Android
+		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, patients);
+		
+		patientsListView.setAdapter(arrayAdapter);
 
-		}
+	}
 
 	private void setupListeners() {
+		
+		ListView patientsListView = (ListView) findViewById(R.id.patients_listView);
+		
+		patientsListView.setOnItemClickListener(new OnItemClickListener() {
+			
+			public void onItemClick(AdapterView parentView, View childView, int position, long id) {
+				
+				Intent intent = new Intent(childView.getContext(), PatientInfoActivity.class);
+				startActivityForResult(intent, 0);
+			}
+
+		});
+		
 		final Button manageButton = (Button) this.findViewById(R.id.ManagePatient_bn);
 		manageButton.setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 
-				Intent intent = new Intent(v.getContext(), PatientInfoActivity.class);
-				startActivityForResult(intent, 0);
-
+				// Pass a new intent to ManagePatientsActivity once it's been written.
+				
 			}
 		});	
 	}
