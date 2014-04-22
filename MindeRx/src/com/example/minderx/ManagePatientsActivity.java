fd3862d5d01b67650dd77a14c6faafc6baf64320
Josh Jones <jwjones5@crimson.ua.edu>
2014-04-22 11:34:46 -0500

    created the Activities correctly (deleting the xml file and creating the blank screen activity) for the following (XMLs): graph_patient_info, manage_patients, patient_blood_pressure, patient_heart_rate

---
+++ MindeRx/src/com/example/minderx/ManagePatientsActivity.java
@@ -0,0 +1,22 @@
+package com.example.minderx;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ManagePatientsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_manage_patients);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.manage_patients, menu);
+		return true;
+	}
+
+}
