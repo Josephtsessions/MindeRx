b7d01855b383315090d6d11b86b0f632c77d3d21
Joseph Sessions <Jtsessions@crimson.ua.edu>
2014-04-22 14:50:09 -0500

    Replaces PatientListActivity with correct code

--- MindeRx/src/com/example/minderx/PatientListActivity.java
+++ MindeRx/src/com/example/minderx/PatientListActivity.java
@@ -1,16 +1,41 @@
 package com.example.minderx;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import database.MindeRxDatabase;
 
 public class PatientListActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_patient_list);
-    }
+    	super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_patient_list);
+		
+		Bundle extras = getIntent().getExtras();
+		String essn = extras.getString("ESSN");
+		
+		setupListeners(essn, queryForPssns(essn));	
+		
+		ListView patientsListView = (ListView) findViewById(R.id.patients_listView);
+		
+		ArrayList<String> patients = queryForPatients(essn);
+		
+		// Adapters are used to populate ListViews in Android
+		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, patients);
+		
+		patientsListView.setAdapter(arrayAdapter);    
+	}
 
 
     @Override
@@ -20,4 +45,45 @@ public class PatientListActivity extends Activity {
         return true;
     }
     
+    private void setupListeners(final String essn, final ArrayList<String> pssns) {
+		
+		ListView patientsListView = (ListView) findViewById(R.id.patients_listView);
+		
+		patientsListView.setOnItemClickListener(new OnItemClickListener() {
+			
+			public void onItemClick(AdapterView parentView, View childView, int position, long id) {
+				
+				Intent intent = new Intent(childView.getContext(), PatientInfoActivity.class);
+				intent.putExtra("ESSN", essn);
+				intent.putExtra("PSSN", pssns.get(position));
+				
+				startActivityForResult(intent, 0);
+			}
+
+		});
+		
+		final Button manageButton = (Button) this.findViewById(R.id.ManagePatient_bn);
+		manageButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				// Pass a new intent to ManagePatientsActivity once it's been written.
+				
+			}
+		});	
+	}
+	
+	private ArrayList<String> queryForPssns(String essn) {
+		MindeRxDatabase db = new MindeRxDatabase(this);
+		
+		return db.getPssnsFromEssn(essn);	
+	}
+	
+	private ArrayList<String> queryForPatients(String essn) {
+		MindeRxDatabase db = new MindeRxDatabase(this);
+		
+		return db.getPatientNamesFromPssns(queryForPssns(essn));
+	}
+	
 }
