b0064a44225f86369c3e54b0c995cadbfa3a11e3
Joseph <Jtsessions@crimson.ua.edu>
2014-04-22 16:55:17 -0500

    Restores PatientInfoActivity functionality

--- MindeRx/src/com/example/minderx/PatientInfoActivity.java
+++ MindeRx/src/com/example/minderx/PatientInfoActivity.java
@@ -1,23 +1,39 @@
 package com.example.minderx;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class PatientInfoActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_patient_info);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_patient_info);
 
+		setupListeners(R.id.blood_pressure_bn, PatientBloodPressureActivity.class);
+		setupListeners(R.id.heart_rate_bn    , PatientHeartRateActivity.class);
+		setupListeners(R.id.o2_sat_lvl_bn    , PatientSaLevelActivity.class);
+		setupListeners(R.id.temperature_bn   , PatientTemperatureActivity.class);	
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.patient_info, menu);
-        return true;
-    }
-    
+	}
+
+	private void setupListeners(int rID, final Class<?> activity) {
+		final Button b = (Button) this.findViewById(rID);
+		b.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				
+				Intent intent = new Intent(v.getContext(), activity);
+				startActivityForResult(intent, 0);
+	
+			}
+		});	
+	}
+	
 }
